f0dfad50ccbaddb8112cc6479a901e785a2aeadc
Pavel_Sakalouski <p.sokolovsky62@gmail.com>
2021-06-06 22:59:45 +0300

    Add Message queues task

---
+++ Message queues/Message queues/ClientProject/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClientProject
+{
+    class Program
+    {
+        const string MessageQueueName = @".\Private$\MySimpleLocalQueue";
+
+        const string ListeningFolderName = @"D:\ListeningFolder";
+
+
+        static void Main(string[] args)
+        {
+            var sdf = new MessageQueue();
+
+            var sentFiles = new List<string>();
+
+            if (!Directory.Exists(ListeningFolderName))
+            {
+                Directory.CreateDirectory(ListeningFolderName);
+            }
+
+            while (true)
+            {
+                var result = Directory.GetFiles(ListeningFolderName);
+
+                foreach (var file in result)
+                {
+                    if (!sentFiles.Contains(file))
+                    {
+                        SendFile(file);
+                        sentFiles.Add(file);
+                    }
+                }
+
+                Thread.Sleep(2000);
+
+            }
+           
+        }
+
+        private static void SendFile(string path)
+        {
+            using (var client = new MessageQueue(MessageQueueName))
+            {
+                var message = new Message();
+
+                using (var fs = new FileStream(path, FileMode.Open))
+                {
+                    message.BodyStream = fs;
+                    message.Label = Path.GetFileName(path);
+                    message.Priority = MessagePriority.Normal;
+                    client.Send(message);
+                }
+                
+            }
+        }
+    }
+}
---
+++ Message queues/Message queues/ServerProject/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerProject
+{
+    public class Program
+    {
+        const string MessageQueueName = @".\Private$\MySimpleLocalQueue";
+
+        private static MessageQueue Queue { get; set; }
+
+        public class ErrorNotification
+        {
+            public int ErrorId { get; set; }
+            public TraceLevel Level { get; set; }
+            public string Message { get; set; }
+
+            public override string ToString()
+            {
+                return ErrorId + " - " + Level + " - " + Message;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (!MessageQueue.Exists(MessageQueueName))
+            {
+                Queue = MessageQueue.Create(MessageQueueName);
+            }
+            else
+            {
+                Queue = new MessageQueue(MessageQueueName);
+            }
+
+            Queue.Formatter = new XmlMessageFormatter(new Type[] { typeof(ErrorNotification), typeof(object) });
+
+            while (true)
+            {
+                var message = Queue.Receive();
+                var buffer = new byte[100];
+                using (var sr = message.BodyStream)
+                {
+                    buffer = new byte[sr.Length];
+                    var sdf  = sr.Read(buffer,0, (int)sr.Length);
+                }
+
+                using (var stream = new FileStream(@"D:\FolderForMessageQueue\" + message.Label, FileMode.OpenOrCreate))
+                {
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+                
+            }
+        }
+    }
+}
